98456f687486d334a0b234efaa502c326a00c239
chentingxin <41343128@nfu.edu.tw>
2025-12-15 15:23:50 +0800

    updata

---
+++ ImageProcessor/imageprocessor.cpp
@@ -0,0 +1,72 @@
+#include "imageprocessor.h"
+#include <QHBoxLayout>
+#include <QMenuBar>
+#include <QFileDialog>
+#include <QDebug>
+
+ImageProcessor::ImageProcessor(QWidget *parent)
+    : QMainWindow(parent)
+{
+    setWindowTitle (QStringLiteral("影像處理"));
+    central =new QWidget();
+    QHBoxLayout *mainLayout = new QHBoxLayout (central);
+    imgWin = new QLabel();
+    QPixmap
+        *initPixmap = new QPixmap(300,200);
+    initPixmap->fill(QColor(255,255,255));
+    imgWin->resize (300,200);
+    imgWin->setScaledContents(true);
+    imgWin->setPixmap (*initPixmap);
+    mainLayout->addWidget (imgWin);
+    setCentralWidget (central);
+    createActions();
+    createMenus();
+    createToolBars();
+}
+
+ImageProcessor::~ImageProcessor() {}
+void ImageProcessor::createActions (){
+    openFileAction=new QAction (QStringLiteral("開啟檔案&0"),this);
+    openFileAction->setShortcut (tr("Ctrl+0"));
+    openFileAction->setStatusTip (QStringLiteral("開啟影像檔案"));
+    connect (openFileAction, SIGNAL (triggered()), this, SLOT (showOpenFile()));
+    exitAction = new QAction (QStringLiteral("結束&Q"),this);
+    exitAction->setShortcut (tr("Ctrl+Q"));
+    exitAction->setStatusTip (QStringLiteral ("退出程式"));
+    connect (exitAction, SIGNAL (triggered()), this, SLOT (close()));
+}
+void ImageProcessor::createMenus (){
+    fileMenu=menuBar ()->addMenu (QStringLiteral("檔案&F"));
+    fileMenu->addAction(openFileAction);
+    fileMenu->addAction (exitAction);
+}
+void ImageProcessor::createToolBars (){}
+void ImageProcessor::loadFile(QString filename){
+    qDebug() <<QString("file name: %1").arg(filename);
+    QByteArray ba=filename.toLatin1();
+    printf("FN:%s\n", (char *) ba.data());
+    img.load(filename);
+    imgWin->setPixmap (QPixmap:: fromImage (img));
+    fileTool=addToolBar("file");
+    fileTool->addAction (openFileAction);
+}
+void ImageProcessor::showOpenFile(){
+    filename=QFileDialog::getOpenFileName (this,
+                                          QStringLiteral("開啟影像"),
+                                          tr("."),
+                                          "bmp(*.bmp);;png(*.png)"
+                                          ";;Jpeg(*.jpg)");
+    if (!filename.isEmpty())
+    {
+        if (img.isNull())
+        {
+            loadFile(filename);
+        }
+        else
+        {
+            ImageProcessor *newIPWin = new ImageProcessor();
+            newIPWin->show();
+            newIPWin->loadFile(filename);
+        }
+    }
+}
---
+++ ImageProcessor/imageprocessor.h
@@ -0,0 +1,34 @@
+#ifndef IMAGEPROCESSOR_H
+#define IMAGEPROCESSOR_H
+
+#include <QMainWindow>
+#include <QACtion>
+#include <QMenu>
+#include <QToolBar>
+#include <QImage>
+#include <QLabel>
+
+class ImageProcessor : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    ImageProcessor(QWidget *parent = nullptr);
+    ~ImageProcessor();
+    void createActions();
+    void createMenus();
+    void createToolBars();
+    void loadFile (QString filename);
+private slots:
+    void showOpenFile();
+private:
+    QWidget *central;
+    QMenu *fileMenu;
+    QToolBar *fileTool;
+    QImage img;
+    QString filename;
+    QLabel *imgWin;
+    QAction *openFileAction;
+    QAction *exitAction;
+};
+#endif // IMAGEPROCESSOR_H
